f1cec98aca0dd0f7ef53b9c7d0d27c4fba8c913c
thelovesmith <averydante9@gmail.com>
2019-10-16 12:48:49 -0400

    setup a basic netlify form with random react hook console logging the state

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,36 +1,62 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useReducer } from 'react';
 import { FormWrapper } from './contact.css.js';
 import { H3, StyledDiv } from '../../styledComps';
 
 const ContactForm = () => {
   const [state, updateState] = useReducer(
     (state, newState) => ({ ...state, ...newState }),
-    { name: '', email: '', password: '' }
+    { name: '', email: '', phone: '', message: '' }
   );
-
+  function formSubmit(e) {
+    e.preventDefault();
+    console.log('recieved form state', state);
+  }
   const { name, email, phone, message } = state;
   return (
     <>
       <StyledDiv>
         <H3>Contact Us</H3>
         <FormWrapper>
-          <form>
+          <form
+            method="post"
+            action="#"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={formSubmit}
+          >
+            <input type="hidden" name="form-name" value="contact" />
             <input
               name="name"
               label="name"
               value={name}
               placeholder="Full Name"
               onChange={e => updateState({ name: e.target.value })}
+              type="text"
             />
             <input
               name="email"
               label="email"
               value={email}
               placeholder="Email Address"
-              onChange={e => useReducer({ email: e.target.value })}
+              onChange={e => updateState({ email: e.target.value })}
             />
-            <input />
-            <input />
+            <input
+              name="phone"
+              label="phone"
+              value={phone}
+              placeholder="xxx-xxx-xxxx"
+              onChange={e => updateState({ phone: e.target.value })}
+            />
+            <textarea
+              name="message"
+              label="message"
+              value={message}
+              placeholder="Please enter your message......"
+              onChange={e => updateState({ message: e.target.value })}
+            ></textarea>
+            <button name="submit" type="submit">
+              Send Message
+            </button>
           </form>
         </FormWrapper>
       </StyledDiv>
