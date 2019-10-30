438f9d0d0227b6343e671cb024d4a959604cb42b
thelovesmith <averydante9@gmail.com>
2019-10-30 10:27:09 -0400

    removed reat-helmet manifest link

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -83,7 +83,7 @@ const Head = ({
       name="msapplication-square310x310"
     />
 
-    <link href="/manifest.json" rel="manifest" />
+    {/* <link href="/manifest.json" rel="manifest" /> */}
 
     <link
       href="/icons/apple-touch-icon-57x57.png"
